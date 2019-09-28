6b35e021f80e3256ce7a14b777f1831523cf36de
aalu1418 <aalu1418@gmail.com>
2019-09-27 20:31:36 -0400

    multiple logos floating now working

--- js/animation.js
+++ js/animation.js
@@ -1,55 +1,45 @@
-const move = () => {
-  anime({
-    targets: '.animation-logo',
-    translateX: nextX(),
-    translateY: nextY(),
-    // duration: calc_duration(),
-    easing: 'linear',
-    complete: move,
-  });
-  dim = (dim === "height") ? "width":"height";
-  width = $(".animation-logo").position().left;
-  height = $(".animation-logo").position().top;
-  // console.log(dim, width, height);
-}
-
-let width = 0;
-let height = 0;
-let newX;
-let newY;
-let dim = "height";
-
-function startAnimation(){
-  max_height = $(".background-animation").height()-$(".continue-bar").height()-$(".animation-logo").height();
-  max_width = $(".background-animation").width()-$(".animation-logo").width();
+function startAnimation(target_obj){
+  let width = 0;
+  let height = 0;
+  let newX;
+  let newY;
+  let dim = "height";
+  const max_height = $(".background-animation").height()-$(".continue-bar").height()-$(".animation-logo").height();
+  const max_width = $(".background-animation").width()-$(".animation-logo").width();
   // console.log(max_height, max_width);
+  const move = () => {
+    anime({
+      targets: target_obj,
+      translateX: nextX(),
+      translateY: nextY(),
+      duration: 3000,
+      easing: 'linear',
+      complete: move,
+    });
+    dim = (dim === "height") ? "width":"height";
+    width = $(target_obj).position().left;
+    height = $(target_obj).position().top;
+    // console.log(dim, width, height);
+  }
+  function nextX(){
+    if (dim === "height"){
+      newX = anime.random(0,max_width);
+    }
+    else {
+      newX = (Math.floor(width) === Math.floor(max_width)) ? 0:max_width
+    }
+    return newX
+  }
+
+  function nextY(){
+    if (dim === "width"){
+      newY = anime.random(0,max_height);
+    }
+    else {
+      newY = (Math.floor(height) === Math.floor(max_height)) ? 0:max_height
+    }
+    return newY
+  }
+
   move();
-}
-
-function nextX(){
-  if (dim === "height"){
-    newX = anime.random(0,max_width);
-  }
-  else {
-    newX = (Math.floor(width) === Math.floor(max_width)) ? 0:max_width
-  }
-  return newX
-}
-
-function nextY(){
-  if (dim === "width"){
-    newY = anime.random(0,max_height);
-  }
-  else {
-    newY = (Math.floor(height) === Math.floor(max_height)) ? 0:max_height
-  }
-  return newY
-}
-
-function calc_duration() {
-  // console.log(newX, newY);
-  const time_conversion = 3;
-  const distance = Math.sqrt(Math.pow(newX-width, 2)+Math.pow(newY-height, 2));
-  console.log(distance, distance*time_conversion);
-  return distance*time_conversion
 }
